b41029d2967e3e58e6662b1405120acac5353fe5
thtngan <thtngan>
2022-05-04 23:46:03 +0700

    Pay receipt with remain amount

--- src/com/kina/login/ChangePwd.java
+++ src/com/kina/login/ChangePwd.java
@@ -13,13 +13,14 @@ import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import org.mindrot.jbcrypt.BCrypt;
 
-public class LoginForm extends javax.swing.JFrame {
+public class ChangePwd extends javax.swing.JFrame {
 
-    /**
-     * Creates new form LoginForm
-     */
-    public LoginForm() {
+    String name;
+    
+    public ChangePwd(String name) {
         initComponents();
+        this.name = name;
+        jLabel6.setText(name);
     }
 
 
@@ -37,10 +38,10 @@ public class LoginForm extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        pwdUser = new javax.swing.JPasswordField();
+        rePwd = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
-        txtUser = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
+        pwd = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -95,13 +96,13 @@ public class LoginForm extends javax.swing.JFrame {
         jPanel3.setBackground(new java.awt.Color(255, 255, 255));
         jPanel3.setLayout(new java.awt.BorderLayout());
 
-        jLabel5.setFont(new java.awt.Font("Arial Black", 1, 36)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Century Gothic", 1, 36)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(65, 105, 225));
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("WELCOME");
         jPanel3.add(jLabel5, java.awt.BorderLayout.CENTER);
 
-        jLabel6.setFont(new java.awt.Font("Arial Black", 0, 24)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Century Gothic", 0, 24)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(65, 105, 225));
         jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel6.setText("LOGIN");
@@ -112,15 +113,15 @@ public class LoginForm extends javax.swing.JFrame {
 
         jPanel4.setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel4.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(65, 105, 225));
-        jLabel4.setText("Password");
+        jLabel4.setText("Re-new Password");
 
-        pwdUser.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        pwdUser.setText("jPasswordField1");
-        pwdUser.addMouseListener(new java.awt.event.MouseAdapter() {
+        rePwd.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        rePwd.setText("jPasswordField1");
+        rePwd.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                pwdUserMouseClicked(evt);
+                rePwdMouseClicked(evt);
             }
         });
 
@@ -133,17 +134,17 @@ public class LoginForm extends javax.swing.JFrame {
             }
         });
 
-        txtUser.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
-        txtUser.setText("identity number ");
-        txtUser.addMouseListener(new java.awt.event.MouseAdapter() {
+        jLabel7.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(65, 105, 225));
+        jLabel7.setText("New Password");
+
+        pwd.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        pwd.setText("jPasswordField1");
+        pwd.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                txtUserMouseClicked(evt);
+                pwdMouseClicked(evt);
             }
         });
-
-        jLabel7.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(65, 105, 225));
-        jLabel7.setText("Number ID");
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
@@ -151,30 +152,27 @@ public class LoginForm extends javax.swing.JFrame {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addGap(15, 15, 15)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(jPanel4Layout.createSequentialGroup()
-                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, 0)
-                        .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel4Layout.createSequentialGroup()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, 0)
-                        .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(pwdUser, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(15, Short.MAX_VALUE))
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(pwd, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rePwd, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addGap(23, 23, 23)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(pwd, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(20, 20, 20)
-                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(pwdUser, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(rePwd, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(38, 38, 38)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(39, Short.MAX_VALUE))
@@ -187,65 +185,28 @@ public class LoginForm extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        String username = txtUser.getText();
-        String password = String.valueOf(pwdUser.getPassword());
-        if (username.equals("") || password.equals("")) {
-            JOptionPane.showMessageDialog(this, "Please enter your NoID & Password", "Error", 1);
+        String password1 = String.valueOf(pwd.getPassword());
+        String password = String.valueOf(rePwd.getPassword());
+
+        
+        if (password1.equals("") || password.equals("") || !password1.equals(password)) {
+            JOptionPane.showMessageDialog(this, "Please enter the same password", "Error", 1);
         } else {
-            connectDB cn = new connectDB();
-            Connection conn = null;
-        try {
-            conn = cn.getConnection();
-            String sql = "SELECT * FROM ACCOUNT WHERE NoID= ?";
-            PreparedStatement pst = conn.prepareStatement(sql);
-            pst.setString (1, username);
-            ResultSet rs = pst.executeQuery();
-            if (rs.next()) {
-                Boolean valuate = BCrypt.checkpw(password, rs.getString("Pass"));
-                if (valuate){
-                    System.out.println(rs.getString("Roles"));
-                    switch(rs.getString("Roles")) {
-                        case "1":                     
-                            this.setVisible(false);
-                            Admin_Main.main();
-                            break;
-                        case "2":
-                            this.setVisible(false);
-                            Manager_Main.main();
-                            break;
-                        case "3": // Login for first tiem
-                            this.setVisible(false);
-//                            User_Main.main(username);
-                            break;
-                        case "4":
-                            this.setVisible(false);
-                            User_Main.main(UserService.getUserID(username));
-                        default:
-                          JOptionPane.showMessageDialog(this, "Your account has been disabled. Please contact admin to reactivate !", "Error", 1);
-                    }
-                } else {
-                    JOptionPane.showMessageDialog(this, "Wrong password", "Error", 1);
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "ID number is not exist", "Error", 1);
-            }            
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(this, ex.toString());
+            
         }
-       }
+               
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void txtUserMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtUserMouseClicked
+    private void rePwdMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_rePwdMouseClicked
         // TODO add your handling code here:
-        txtUser.setText("");
-    }//GEN-LAST:event_txtUserMouseClicked
+        rePwd.setText("");
+    }//GEN-LAST:event_rePwdMouseClicked
 
-    private void pwdUserMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pwdUserMouseClicked
+    private void pwdMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pwdMouseClicked
         // TODO add your handling code here:
-        pwdUser.setText("");
-    }//GEN-LAST:event_pwdUserMouseClicked
+    }//GEN-LAST:event_pwdMouseClicked
 
-    public static void main() {
+    public static void main(String name) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -259,26 +220,14 @@ public class LoginForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -287,7 +236,7 @@ public class LoginForm extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LoginForm().setVisible(true);
+                new ChangePwd(name).setVisible(true);
             }
         });
     }
@@ -304,7 +253,7 @@ public class LoginForm extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JPasswordField pwdUser;
-    private javax.swing.JTextField txtUser;
+    private javax.swing.JPasswordField pwd;
+    private javax.swing.JPasswordField rePwd;
     // End of variables declaration//GEN-END:variables
 }
--- src/com/kina/main/ManagementCovid.java
+++ src/com/kina/main/ManagementCovid.java
@@ -1,6 +1,7 @@
 package com.kina.main;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.kina.login.ChangePwd;
 import com.kina.login.LoginForm;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -14,8 +15,8 @@ public class ManagementCovid {
             UIManager.setLookAndFeel(new FlatIntelliJLaf());
 //            LoginForm.main();
 //            Admin_Main.main();
-            Manager_Main.main();
-//              User_Main.main("BN0001");
+//            Manager_Main.main();
+              User_Main.main("BN0001");
 
             
             
